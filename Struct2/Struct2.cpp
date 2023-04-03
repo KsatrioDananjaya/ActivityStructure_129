5f9dc86e5c3995de0feebf1d43be8efa7e99b5f1
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-04-03 14:32:39 +0700

    membuat struct ke 2 dengan alamat detail

---
+++ Struct2/Struct2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct AlamatDetail {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
+
+int main() {
+
+}
