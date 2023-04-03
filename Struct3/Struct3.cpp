4b6f56d8e521deea559fddce273e0c7f4fd194ca
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-04-03 14:41:26 +0700

    membuat struct terakhir yang menggunakan array

---
+++ Struct3/Struct3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct AlamatDetail {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
