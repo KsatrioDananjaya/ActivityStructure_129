8802e5a99f9cd238c540812ef31e4a940927158a
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-04-03 14:42:58 +0700

    menambahkan array ke mahasiswa

--- Struct3/Struct3.cpp
+++ Struct3/Struct3.cpp
@@ -13,3 +13,20 @@ struct Mahasiswa {
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+
+	Mahasiswa mhs[3];
+
+	cout << "Masukkan NIM : ";
+	cin >> mhs[i].nim;
+	cout << "Masukkan Nama : ";
+	cin >> mhs[i].nama;
+	cout << "Alamat : " << endl;
+	cout << "\tMasukkan Desa : ";
+	cin >> mhs[i].alamat.desa;
+	cout << "\tMasukkan Kota : ";
+	cin >> mhs[i].alamat.kota;
+	cout << "Masukkan Umur : ";
+	cin >> mhs[i].umur;
+}
